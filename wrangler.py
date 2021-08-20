a4a66b1dd40e950c5f263b8a53b63da7d813e4cf
Jonathan Chau <31170739+RadEagle@users.noreply.github.com>
2021-08-19 20:21:10 -0700

    Update wrangler.py

--- wrangler.py
+++ wrangler.py
@@ -1,5 +1,6 @@
 # Wrangle a JSON database to suit website needs
 # original data from https://github.com/davidluzgouveia/kanji-data
+# hyogai and jinmeiyo extension from https://github.com/mifunetoshiro/kanjium/blob/master/data/kanjidb.sqlite
 import json
 
 # Open the JSON file
@@ -8,7 +9,7 @@ file_name = "kanji-jouyou.json"
 json_file = open(file_name, 'r')
 
 # Set up output file
-out_file = open("kanji_3.json", 'w')
+out_file = open("kanji_4.json", 'w')
 
 # Load the file into a JSON object
 print('setting up json...')
@@ -23,8 +24,8 @@ filler = ["jlpt_old", "jlpt_new", "meanings", "readings_on", "readings_kun",
 # set new values if null
 book = dict()
 book["freq"] = 2500
-book["jlpt_new"] = 0
-book["wk_level"] = 70
+book["jlpt_new"] = 1
+book["wk_level"] = 61
 
 # modify the JSON data
 print('wrangling data...')
@@ -40,11 +41,40 @@ for kanji in kanji_list:
     item["jlpt"] = 6 - item["jlpt_new"]
 
     # make a new wk category (wk = wk_level / 10)
-    item["wk"] = item["wk_level"] / 10 + 1
+    item["wk"] = item["wk_level"] / 10
 
     # delete categories to clean up data
     for category in filler:
         del item[category]
+
+
+############################################################
+# NEW version 1.1 (adding jinmeiyo and hyogai kanji)
+############################################################
+def fill_in_kanji(source, destination, grade, jlpt, wk):
+    for obj in source:
+        kanji = obj['kanji']
+        try:
+            print(destination[kanji], kanji)
+        except KeyError:
+            destination[kanji] = {}
+            destination[kanji]["grade"] = grade
+            destination[kanji]["jlpt"] = jlpt
+            destination[kanji]["wk"] = wk
+
+
+jinmeiyo_name = "kanji-jinmeiyo.json"
+jinmeiyo_file = open(jinmeiyo_name, 'r')
+jinmeiyo_json = json.load(jinmeiyo_file)
+
+hyogai_name = "kanji-hyogai.json"
+hyogai_file = open(hyogai_name, 'r')
+hyogai_json = json.load(hyogai_file)
+
+fill_in_kanji(hyogai_json, kanji_json, 9, 6, 7)
+fill_in_kanji(jinmeiyo_json, kanji_json, 10, 7, 8)
+
+############################################################
 
 # dump the json to the output file
 # make sure to keep kanji in their original form
@@ -56,4 +86,6 @@ out_file.write(pretty_json.decode())
 # close the files
 print('closing files...')
 json_file.close()
+jinmeiyo_file.close()
+hyogai_file.close()
 out_file.close()
